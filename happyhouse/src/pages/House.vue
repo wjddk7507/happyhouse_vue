4c4b31c2cee1a379fefbd3c2ceb0c5f749ea6041
이정아 <wjddk7507@naver.com>
2021-11-18 15:52:09 +0900

    house 복붙

---
+++ happyhouse/src/pages/House.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100">
+        <house-search-bar></house-search-bar>
+        <house-list />
+        <house-detail />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HouseSearchBar from "@/pages/House/HouseSearchBar.vue";
+import HouseList from "@/pages/House/HouseList.vue";
+import HouseDetail from "@/pages/House/HouseDetail.vue";
+
+export default {
+  name: "House",
+  components: {
+    HouseSearchBar,
+    HouseList,
+    HouseDetail,
+  },
+};
+</script>
