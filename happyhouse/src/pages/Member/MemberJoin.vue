5704b199dee6f95a7f87345972576ae455854da8
이정아 <wjddk7507@naver.com>
2021-11-24 14:34:09 +0900

    회원가입시 공란 체크, id 중복 체크, 비밀번호 길이 체크

--- happyhouse/src/pages/Member/MemberJoin.vue
+++ happyhouse/src/pages/Member/MemberJoin.vue
@@ -9,64 +9,68 @@
       <md-card-content>
         <div class="md-layout">
           <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field :class="messageClass">
+            <md-field :class="idClass">
               <label>아이디</label>
-              <md-input v-model="userid" type="text"></md-input>
-              <span class="md-error">There is an error</span>
+              <md-input
+                ref="userid"
+                v-model="userid"
+                type="text"
+                @input="idCheck"
+              ></md-input>
+              <span class="md-error">이미 존재하는 ID 입니다.</span>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50"></div>
           <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
+            <md-field :class="pwdClass">
               <label>비밀번호</label>
-              <md-input v-model="userpwd" type="password"></md-input>
+              <md-input
+                id="userpwd"
+                ref="userpwd"
+                v-model="userpwd"
+                type="password"
+                @input="pwdCheck"
+              ></md-input>
+              <span class="md-error">비밀번호는 8자 이상입니다.</span>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
+            <md-field :class="pwdchkClass">
               <label>비밀번호 재확인</label>
-              <md-input v-model="pwdchk" type="password"></md-input>
+              <md-input
+                ref="pwdchk"
+                v-model="pwdchk"
+                type="password" @input="pwdchkCheck"
+              ></md-input>
+              <span class="md-error">동일한 비밀번호를 다시 입력해주세요.</span>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
+            <md-field :class="nameClass">
               <label>이름</label>
-              <md-input v-model="username" type="text"></md-input>
+              <md-input
+                ref="username"
+                v-model="username"
+                type="text"
+              ></md-input>
+              <span class="md-error">이름을 입력해주세요.</span>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50"></div>
-<!-- 
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>생년</label>
-              <md-input v-model="year" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>월</label>
-              <md-input v-model="month" type="text"></md-input>
-            </md-field>
-          </div>
-          <div class="md-layout-item md-small-size-100 md-size-33">
-            <md-field>
-              <label>일</label>
-              <md-input v-model="day" type="text"></md-input>
-            </md-field>
-          </div> -->
-
           <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
+            <md-field :class="emailClass">
               <label>본인확인 이메일</label>
-              <md-input v-model="email" type="email"></md-input>
+              <md-input ref="email" v-model="email" type="email"></md-input>
+              <span class="md-error">이메일을 입력해주세요.</span>
             </md-field>
           </div>
           <div class="md-layout-item md-small-size-100 md-size-50"></div>
 
           <div class="md-layout-item md-small-size-100 md-size-50">
-            <md-field>
+            <md-field :class="phoneClass">
               <label>휴대전화</label>
-              <md-input v-model="phone" type="number"></md-input>
+              <md-input ref="phone" v-model="phone" type="number"></md-input>
+              <span class="md-error">전화번호를 입력해주세요.</span>
             </md-field>
           </div>
           <div class="md-layout-item md-size-100 text-right">
@@ -74,9 +78,6 @@
               >Join</md-button
             >
           </div>
-          <md-switch v-model="hasMessages"
-            >{{ !hasMessages ? "Show" : "Hide" }} Errors</md-switch
-          >
         </div>
       </md-card-content>
     </md-card>
@@ -96,37 +97,138 @@ export default {
       email: "",
       phone: "",
       pwdchk: "",
-      // year: "",
-      // month: "",
-      // day: "",
-      hasMessages: false,
+      idMsg: false,
+      pwdMsg: false,
+      pwdchkMsg: false,
+      nameMsg: false,
+      emailMsg: false,
+      phoneMsg: false,
     };
   },
   computed: {
-    messageClass() {
+    idClass() {
       return {
-        "md-invalid": this.hasMessages,
+        "md-invalid": this.idMsg,
+      };
+    },
+    pwdClass() {
+      return {
+        "md-invalid": this.pwdMsg,
+      };
+    },
+    pwdchkClass() {
+      return {
+        "md-invalid": this.pwdchkMsg,
+      };
+    },
+    nameClass() {
+      return {
+        "md-invalid": this.nameMsg,
+      };
+    },
+    emailClass() {
+      return {
+        "md-invalid": this.emailMsg,
+      };
+    },
+    phoneClass() {
+      return {
+        "md-invalid": this.phoneMsg,
       };
     },
   },
   methods: {
     joinMember() {
+      let err = true;
+      let msg = "";
+
+      !this.userid && ((err = false), this.$refs.userid.$el.focus());
+      err &&
+        (!this.userpwd || this.userpwd.length < 8) &&
+        ((err = false),
+        this.$refs.userpwd.$el.focus(),
+        (this.idMsg = false),
+        (this.pwdMsg = true));
+      err &&
+        (!this.pwdchk || this.userpwd !== this.pwdchk) &&
+        ((err = false),
+        this.$refs.pwdchk.$el.focus(),
+        (this.idMsg = false),
+        (this.pwdMsg = false),
+        (this.pwdchkMsg = true));
+      err &&
+        !this.username &&
+        ((err = false),
+        this.$refs.username.$el.focus(),
+        (this.idMsg = false),
+        (this.pwdMsg = false),
+        (this.pwdchkMsg = false),
+        (this.nameMsg = true));
+      err &&
+        !this.email &&
+        ((err = false),
+        this.$refs.email.$el.focus(),
+        (this.idMsg = false),
+        (this.pwdMsg = false),
+        (this.pwdchkMsg = false),
+        (this.nameMsg = false),
+        (this.emailMsg = true));
+      err &&
+        !this.phone &&
+        ((err = false),
+        this.$refs.phone.$el.focus(),
+        (this.idMsg = false),
+        (this.pwdMsg = false),
+        (this.pwdchkMsg = false),
+        (this.nameMsg = false),
+        (this.emailMsg = false),
+        (this.phoneMsg = true));
+
+      if (err) {
+        http
+          .post(`/user/register`, {
+            userid: this.userid,
+            userpwd: this.userpwd,
+            username: this.username,
+            email: this.email,
+            phone: this.phone,
+          })
+          .then(({ data }) => {
+            let msg = "등록 처리시 문제가 발생했습니다.";
+            if (data === "success") {
+              msg = "등록이 완료되었습니다.";
+            }
+            alert(msg);
+            this.$router.push({ name: "Home" });
+          });
+      }
+    },
+    idCheck() {
       http
-        .post(`/user/register`, {
-          userid: this.userid,
-          userpwd: this.userpwd,
-          username: this.username,
-          email: this.email,
-          phone: this.phone,
+        .get(`/user/idcheck`, {
+          params: { ckid: this.userid },
         })
         .then(({ data }) => {
-          let msg = "등록 처리시 문제가 발생했습니다.";
-          if (data === "success") {
-            msg = "등록이 완료되었습니다.";
+          if (data == 0) {
+            this.idMsg = false;
+          } else {
+            this.idMsg = true;
           }
-          alert(msg);
-          this.$router.push({ name: "Home" });
         });
+    },
+    pwdCheck() {
+      if(this.userpwd.length < 8) {
+            this.pwdMsg = true;
+      } else {
+            this.pwdMsg = false;
+      }
+    },
+    pwdchkCheck() {
+      if(this.pwdchk.length < 8 || this.userpwd !== this.pwdchk) {
+            this.pwdchkMsg = true;
+      } else {
+            this.pwdchkMsg = false;
+      }
     },
   },
 };
