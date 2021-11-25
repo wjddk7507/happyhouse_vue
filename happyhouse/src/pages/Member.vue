24250480ee4b2499f18310f79b00ebb78a6c3824
wjddk7507 <wjddk9494@gmail.com>
2021-11-25 14:19:17 +0900

    지도위치 변경 및 페이지마다 여백 설정

--- happyhouse/src/pages/Member.vue
+++ happyhouse/src/pages/Member.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="content">
-    <router-view></router-view>
+    <div class="md-layout">
+      <div
+        class="md-layout-item md-medium-size-15 md-xsmall-size-15 md-size-15"
+      ></div>
+      <div
+        class="md-layout-item md-medium-size-70 md-xsmall-size-70 md-size-70"
+      >
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
